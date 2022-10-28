b237c3190b70955c1273c849ed08315e2c53796f
Alessandro Chilà <alessandro.chila@playground.it>
2022-10-28 10:18:13 +0200

    FIXING

--- assets/section-product-slider.js
+++ assets/section-product-slider.js
@@ -1,30 +1,33 @@
 
 (function (window) {
+        
         if (window.customProductSlider) return;
+
+        class CustomProductSlider {
+
+
+                sliders_aa = null
+
+                constructor() {
+
+                }
+
+                init() {
+                        sliders_aa = document.querySelectorAll('.product-slider');
+                        [...sliders_aa].forEach((slider) => {
+
+                                console.log("spread forEach worked");
+
+                        });
+                }
+
+
+        }
         // now you're certain there's no global module loaded
         let customProductSlider = window.customProductSlider = new CustomProductSlider()
         customProductSlider.init()
         return;
 })(window);
 
-class CustomProductSlider{
 
-
-        sliders_aa = null
-        
-        constructor(){
-
-        }
-
-         init(){
-                sliders_aa = document.querySelectorAll('.product-slider');
-                [...sliders_aa].forEach((slider) => {
-
-                        console.log("spread forEach worked");
-
-                });
-         }
-
-
-}
 
